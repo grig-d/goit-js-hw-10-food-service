d1e8d275ac02cd1b19d4bde264e9ed8e0d4aee8a
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-03-03 03:46:15 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 import './styles.css';
+
+// 
+setUpTheme();
+
+function setUpTheme() {
+  const savedTheme = localStorage.getItem('savedTheme');
+    if (savedTheme) {
+      
+// if(savedTheme === )
+
+    // refs.textarea.value = savedTheme;
+  }
+}
+
+// 
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const refs = {
+    themeToggle: document.querySelector('#theme-switch-toggle'),
+}
+
+console.log(refs.theme);
+
+// refs.theme.addEventListener('change', )
+
+// localStorage.setItem('key', 'value');
+// const value = localStorage.getItem('key');
+// console.log('value from localStorage: ', value);
+
+// const settings = {
+//   theme: 'dark',
+//   fontSize: 14,
+// };
+
+// localStorage.setItem('settings', JSON.stringify(settings));
+
+
