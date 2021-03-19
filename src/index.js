55bd9e05a7b5c78598ede11013d6d1455ecbef11
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-03-19 02:48:26 +0200

    theme toggle done

--- src/index.js
+++ src/index.js
@@ -1,45 +1,42 @@
 import './styles.css';
 
-// 
-// click on button
-// toggle classList
-// 
-setUpTheme();
+const refs = {
+  body: document.querySelector('body'),
+  themeToggle: document.querySelector('#theme-switch-toggle'),
+};
 
-function setUpTheme() {
-  const savedTheme = localStorage.getItem('savedTheme');
-    if (savedTheme) {
-      
-// if(savedTheme === )
-
-    // refs.textarea.value = savedTheme;
-  }
-}
-
-// 
-
+// theme list
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-const refs = {
-    themeToggle: document.querySelector('#theme-switch-toggle'),
+// default or selected Theme
+let currentTheme = !!localStorage.getItem('Theme')
+  ? localStorage.getItem('Theme')
+  : Theme.LIGHT;
+
+// set current theme and checkbox status
+refs.body.classList.add(currentTheme);
+refs.themeToggle.checked = currentTheme === Theme.DARK;
+
+refs.themeToggle.addEventListener('change', setUpTheme);
+
+function setUpTheme() {
+  if (refs.themeToggle.checked) {
+    refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('Theme', Theme.DARK);
+  } else {
+    refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('Theme', Theme.LIGHT);
+  }
 }
 
-console.log(refs.theme);
-
-// refs.theme.addEventListener('change', )
-
-// localStorage.setItem('key', 'value');
-// const value = localStorage.getItem('key');
-// console.log('value from localStorage: ', value);
-
-// const settings = {
-//   theme: 'dark',
-//   fontSize: 14,
-// };
-
-// localStorage.setItem('settings', JSON.stringify(settings));
-
-
+// TASK // ------------------------------------------------------------------------
+// Создай страницу меню с возможностью выбора темы для сервиса заказа еды
+// Добавь функционал изменения темы при нажатии (событие change) на чекбокс #theme-switch-toggle в тулбаре.
+// По умолчанию тема светлая.
+// При изменении темы, необходимо добавлять на элемент body класс light-theme или dark-theme.
+// Выбранная тема должна сохраняться между перезагрузками страницы. Для хранения темы используй localStorage.
+// Если при загрузке страницы тема тёмная, не забудь поставить свойство checked у чекбокса #theme-switch-toggle в true,
+// чтобы ползунок сдвинулся в правильное положение.
